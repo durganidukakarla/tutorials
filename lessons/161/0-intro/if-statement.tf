f277c857a980100aeb27ad525ed5264db7d5f19e
durganidukakarla <durganaidu.azure@gmail.com>
2024-02-09 10:44:27 +0530

    fixthe syntax error in the if-statement.tf file

--- lessons/161/0-intro/if-statement.tf
+++ lessons/161/0-intro/if-statement.tf
@@ -1,7 +1,7 @@
 module "cluster_autoscaler" {
   source = "../../../infrastructure-modules/kubernetes-addons"
 
-  if env == "prodcution" {
+  if env == "production" {
     env         = "production"
     eks_name    = "demo"
     helm_verion = "9.28.0"      
